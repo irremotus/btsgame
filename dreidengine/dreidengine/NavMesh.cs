7b320bcecb75d825c45ae3458b7fbe456d701f82
Pascal Roessner <pythonmolurus95@gmail.com>
2014-04-29 22:41:44 -0600

    added navMesh

---
+++ dreidengine/dreidengine/NavMesh.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using JigLibX.Physics;
+using JigLibX.Geometry;
+using JigLibX.Collision;
+
+
+namespace dreidengine
+{
+    class NavMesh : DrawableGameComponent
+    {
+        Model navMeshModel;
+        private string navMeshName;
+        private HeightMapInfo heihgtMapInfo;
+        public HeightMapInfo HMI { get { return heihgtMapInfo; } }
+
+        public NavMesh(Game game, string name)
+            : base(game)
+        {
+            navMeshName = name;
+        }
+
+        protected override void LoadContent()
+        {
+            navMeshModel = Game.Content.Load<Model>(navMeshName);
+            heihgtMapInfo = navMeshModel.Tag as HeightMapInfo;
+            base.LoadContent();
+        }
+    }
+}
