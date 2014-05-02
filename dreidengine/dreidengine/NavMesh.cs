d43f6ec06f6697f1bf10cefc299c9d092659efcf
Pascal Roessner <pythonmolurus95@gmail.com>
2014-05-02 10:29:03 -0600

    made navmesh visable for debugging purposes

--- dreidengine/dreidengine/NavMesh.cs
+++ dreidengine/dreidengine/NavMesh.cs
@@ -30,5 +30,41 @@ namespace dreidengine
             heihgtMapInfo = navMeshModel.Tag as HeightMapInfo;
             base.LoadContent();
         }
+        private Matrix GetWorldMatrix()
+        {
+            return
+                Matrix.CreateScale(Vector3.One) *
+                Matrix.CreateTranslation(Vector3.Zero);
+        }
+        public override void Draw(GameTime gameTime)
+        {
+            Game1 game = (Game1)Game;
+            this.Game.GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+
+            this.Game.GraphicsDevice.BlendState = BlendState.Opaque;
+            this.Game.GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+            this.Game.GraphicsDevice.SamplerStates[0] = SamplerState.LinearWrap;
+
+
+            Matrix[] transforms = new Matrix[navMeshModel.Bones.Count];
+            navMeshModel.CopyAbsoluteBoneTransformsTo(transforms);
+
+            Matrix worldMatrix = GetWorldMatrix();
+
+            foreach (ModelMesh mesh in navMeshModel.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.PreferPerPixelLighting = true;
+                    effect.World = transforms[mesh.ParentBone.Index] * worldMatrix;
+                    effect.View = game.Camera.View;
+                    effect.Projection = game.Camera.Projection;
+                }
+                mesh.Draw();
+            }
+
+            base.Draw(gameTime);
+        }
     }
 }
